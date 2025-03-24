044d3643ae1ce42374e128a434a57b069fa90165
Yan Victor - LaptopIB <ybritogomes@gmail.com>
2025-03-24 15:11:29 -0300

    feat(pokemonSearch): Added pokémon search with PokéApi API

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent, useState } from 'react';
+
+import './App.css';
+
+interface PokemonInfoInterface {
+  name: string,
+  id: number,
+  weight: number,
+  height: number,
+  sprite: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonName, setPokemonName] = useState<string>();
+  const [pokemonInfo, setPokemonInfo] = useState<PokemonInfoInterface>();
+
+  function pokemonSubmitHandler(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    setPokemonInfo(undefined);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(response => response.json())
+    .then(json => {
+      setPokemonInfo({
+        name: json.name,
+        id: json.id,
+        weight: json.weight,
+        height: json.height,
+        sprite: json.sprites.front_default
+      });
+    });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header id="header">
+        <h1>Pokédex</h1>
+      </header>
+      <main>
+        <form
+          onSubmit={pokemonSubmitHandler}
+          id="pokedexForm"
+        >
+          <input
+            type="text"
+            id="pokemonNameInput"
+            onChange={e => setPokemonName(e.target.value ?? undefined)}
+          />
+          <button
+            id="submitButton"
+            type="submit"
+          >Search</button>
+        </form>
+        {pokemonInfo ? (
+          <section id="searchResult">
+            <img
+              src={pokemonInfo.sprite}
+              id="pokemonSprite"
+            />
+            <p><strong>Name:</strong> {pokemonInfo.name}</p>
+            <p><strong>N°:</strong> {pokemonInfo.id}</p>
+            <p><strong>Weight:</strong> {pokemonInfo.weight / 10}kg</p>
+            <p><strong>Height:</strong> {pokemonInfo.height * 10}cm</p>
+          </section>
+        ) : (
+          <></>
+        )}
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
